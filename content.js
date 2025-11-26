a909691dd411eeb4305021fdaaf2a65e11f74423
floto <settheskibidi@gmail.com>
2025-11-26 09:54:20 -0500

    feat: initial chrome extension with monkey/currency cheats for minky.figma.site

---
+++ content.js
@@ -0,0 +1,12 @@
+// Content script to manipulate the page for cheats
+const clickPower = 100;
+const rebirthCount = 0;
+const rebirthMultiplier = 1;
+const upgrades = [];
+
+function applyCheats() {
+    console.log('Applying cheats for Minky!');
+    // logic to apply cheats to the game
+}
+
+applyCheats();
