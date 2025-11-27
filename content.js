ba9dcdcf35f4d93d39acefe4029949699049d45b
floto <settheskibidi@gmail.com>
2025-11-26 19:10:49 -0500

    fix: tested Chrome extension for cheat panel on minky.figma.site

--- content.js
+++ content.js
@@ -1,12 +1,2 @@
-// Content script to manipulate the page for cheats
-const clickPower = 100;
-const rebirthCount = 0;
-const rebirthMultiplier = 1;
-const upgrades = [];
-
-function applyCheats() {
-    console.log('Applying cheats for Minky!');
-    // logic to apply cheats to the game
-}
-
-applyCheats();
+// Content script that runs on figma.com
+console.log('Cheat Panel loaded'); // Example script content here
